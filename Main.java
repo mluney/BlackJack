52865d2ee3cdb87c9cfb3800ad6d913040b2fe38
Nick <Nick@10.120.23.197>
2018-05-02 12:24:13 -0400

    Tested Deck Class

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ package blackjack;
 public class Main {
 
     public static void main(String[] args) {
-        
+        /* Testing Deck
+        Deck testDeck = new Deck();
+        System.out.println(testDeck.getC());
+        testDeck.shuffle();
+        testDeck.getNextCard();*/
     }
 }
